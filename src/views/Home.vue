e0fb6996f34c5cb649cb649f90af48f9444c2173
qinzhiyonyan <1447312816@qq.com>
2021-02-24 11:11:31 +0800

    大屏适配

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
-    aaa
+    <h1 class="title">大屏脚手架</h1>
+    <p class="text">文本REM</p>
   </div>
 </template>
 
@@ -10,3 +11,16 @@ export default {
   components: {}
 };
 </script>
+
+<style lang="scss" scoped>
+.title {
+  width: 400px;
+  height: 100px;
+  font-size: 32px;
+}
+.text {
+  width: 200px;
+  height: 200px;
+  border-radius: 50%;
+}
+</style>
---
+++ vue.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  css: {
+    sourceMap: true,
+    loaderOptions: {
+      postcss: {
+        plugins: [
+          require("postcss-px2rem")({
+            // 设计稿为750px时为75
+            // 设计图的宽度/10 比如你的设计图是1920的宽度 这里你就1920/10=192
+            remUnit: 75
+          })
+        ]
+      }
+    },
+    requireModuleExtension: true
+  }
+};
