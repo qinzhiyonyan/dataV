540c0977ef10952a40f2d8e8b22167a91de170ee
qinzhiyonyan <1447312816@qq.com>
2021-08-01 16:05:58 +0800

    新增drawio流程图

---
+++ src/views/draw.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <iframe
+      :src="url"
+      ref="iframe"
+      style="height: 800px; width:100%; frameborder=no"
+      frameborder="no"
+      border="0"
+      marginwidth="0"
+      marginheight="0"
+    ></iframe>
+  </div>
+</template>
+<script>
+export default {
+  name: "draw",
+  data() {
+    return {
+      url: "http://172.30.103.158:8081/drawio/"
+    };
+  }
+};
+</script>
